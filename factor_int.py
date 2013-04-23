0a1a72b5ff0de9b8af47cb9ac60d085574e02363
susuk4 <susuk4@uw.edu>
2013-04-22 21:23:51 -0700

    Update and rename Cythonization.pyx to factor_int.py

---
+++ factor_int.py
@@ -0,0 +1,32 @@
+def isPrime(testNumber):
+    for prime in [2,3,5,7,11]:
+        if testNumber%prime==0:
+            return False
+    else:
+        return True
+
+def factor_int(n):
+    n = n
+    lastnumber = str(n)
+    remainder = n
+    returnStr = ""
+    #exception handler
+    if n==0:
+        raise ValueError, "0 cannot be factored"
+    elif isPrime(n):
+        raise ValueError, str(self.n)+" is already a prime number"
+    denominator = [2,3,5,7,11]
+    for de in denominator:
+        while remainder%de==0:
+            remainder = remainder/de
+            lastnumber = str(remainder)
+            returnStr = returnStr + str(de)
+            if remainder != de and remainder != 1 and remainder != -1:
+                returnStr = returnStr+"*"
+            else:
+                break
+    if remainder != 1 and remainder != -1:
+        returnStr = returnStr+str(remainder)
+    elif remainder == -1:
+        returnStr = "-1*"+returnStr
+    return returnStr
