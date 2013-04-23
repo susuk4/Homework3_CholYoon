af0e1947e752c1366fce632b8b87ac3dd06f86df
susuk4 <susuk4@uw.edu>
2013-04-22 23:09:06 -0700

    Update series_sum_sq_p2.pyx

--- series_sum_sq_p2.pyx
+++ series_sum_sq_p2.pyx
@@ -1,7 +1,7 @@
 %cython
 def series_sum_sq(int n):
     cdef int number = n
-    cdef int totalsum
+    cdef long int totalsum
     if number>0:
         totalsum = 0
         for i in range(1,n,1):
@@ -14,3 +14,5 @@ def series_sum_sq(int n):
         raise ValueError, str(n)+" is not positive integer"
     else: 
         raise ValueError, str(n)+" is not an integer"
+       
+#625 loops, best of 3: 912 µs per loop
