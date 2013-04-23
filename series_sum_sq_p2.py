e84f872fd960b4298f799e324aec42bb3d88ba6a
susuk4 <susuk4@uw.edu>
2013-04-22 22:49:23 -0700

    Update series_sum_sq_p2.py

--- series_sum_sq_p2.py
+++ series_sum_sq_p2.py
@@ -1,9 +1,16 @@
 def series_sum_sq(n):
-    totalsum = 0
-    for i in range(1,n,1):
-        totalsum = totalsum + i**2
-    return totalsum
-
+    if n>0:
+        totalsum = 0
+        for i in range(1,n,1):
+            totalsum = totalsum + i**2
+        return totalsum
+    elif n==0:
+        totalsum = 0
+        return totalsum
+    elif n<0:
+        raise ValueError, str(n)+" is not positive integer"
+    else: 
+        raise ValueError, str(n)+" is not an integer"
 #Time n = 100000
-#100 loops, best of 3: 5.9 ms per loop
+#100 loops, best of 3: 5.58 ms per loop
 %timeit(series_sum_sq(10000))
